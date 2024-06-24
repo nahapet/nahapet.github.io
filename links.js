d3eb2c5cf825d2d97896d0a84c98fce0f5d1e4b1
Zaven Nahapetyan <znahapet@gmail.com>
2024-06-23 23:51:20 -0400

    First version

---
+++ links.js
@@ -0,0 +1,109 @@
+// Media coverage links
+
+const links = [
+  {
+    alt: "Yahoo Finance",
+    src: "yahoo-finance.png",
+    href: "https://uk.finance.yahoo.com/news/facebook-web-3-challengers-revolutionise-social-media-083518898.html",
+  },
+  {
+    alt: "Digiday",
+    src: "digiday.svg",
+    href: "https://digiday.com/media/niche-decentralized-social-media/",
+  },
+  {
+    alt: "VML",
+    src: "vml.png",
+    href: "https://www.vml.com/insight/top-5-web3-brands",
+  },
+  {
+    alt: "Fast Company",
+    src: "fast-company.png",
+    href: "https://www.fastcompany.com/90775031/these-facebook-and-tinder-vets-are-building-a-web3-social-network-called-niche",
+  },
+  {
+    alt: "Dot.la",
+    src: "dot-la.webp",
+    href: "https://dot.la/niche-raises-1m-web3-2657802594.html",
+  },
+  {
+    alt: "BuzzFeed News",
+    src: "buzzfeed-news.png",
+    href: "https://www.buzzfeednews.com/article/ryanmac/facebook-republican-states-stop-register-millions-voters",
+  },
+  {
+    alt: "USA Today",
+    src: "usa-today.png",
+    href: "https://www.usatoday.com/story/tech/2020/10/26/voter-registration-facebook-drive-trump-biden-zuckerberg-election-2020/6041428002/",
+  },
+  {
+    alt: "CNBC",
+    src: "cnbc.png",
+    href: "https://www.cnbc.com/2020/10/26/facebook-helped-4point4-million-people-register-to-vote-in-the-us-this-year.html",
+  },
+  {
+    alt: "Wall Street Journal",
+    src: "wsj.png",
+    href: "https://www.wsj.com/articles/facebook-aims-to-register-4-million-u-s-voters-11592359467",
+  },
+  {
+    alt: "Adweek",
+    src: "adweek.png",
+    href: "https://www.adweek.com/performance-marketing/instagram-heres-how-to-use-the-register-to-vote-sticker-in-stories/",
+  },
+  {
+    alt: "Teen Vogue",
+    src: "teen-vogue.svg",
+    href: "https://www.teenvogue.com/story/instagram-stories-i-voted-sticker-1000-users-per-minute",
+  },
+  {
+    alt: "Elite Daily",
+    src: "elite-daily.png",
+    href: "https://www.elitedaily.com/p/heres-how-to-add-instagrams-2020-i-voted-stickers-to-your-story-to-mark-the-occasion-34083934",
+  },
+  {
+    alt: "Mashable",
+    src: "mashable.png",
+    href: "https://mashable.com/article/instagram-i-voted-stickers-and-stories",
+  },
+  {
+    alt: "Engadget",
+    src: "engadget.png",
+    href: "https://www.engadget.com/2019-09-19-facebook-2-billion-raised-for-causess.html",
+  },
+  {
+    alt: "Forbes",
+    src: "forbes.png",
+    href: "https://www.forbes.com/sites/chrisstrub/2019/10/04/2billion/?sh=4842478416f9",
+  },
+  {
+    alt: "TechCrunch",
+    src: "techcrunch.svg",
+    href: "https://techcrunch.com/2020/11/23/facebook-launches-drives-a-u-s-only-feature-for-collecting-food-clothing-and-other-necessities-for-people-in-need/",
+  },
+  {
+    alt: "Business Insider",
+    src: "business-insider.png",
+    href: "https://www.businessinsider.com/facebook-launches-fundraising-tool-2015-11",
+  },
+  {
+    alt: "CNET",
+    src: "cnet.png",
+    href: "https://www.cnet.com/tech/tech-industry/facebook-built-a-new-center-to-direct-people-to-accurate-coronavirus-information/",
+  },
+  {
+    alt: "New York Times",
+    src: "new-york-times.png",
+    href: "https://www.nytimes.com/2015/04/28/world/asia/google-and-facebook-help-nepal-earthquake-survivors-and-contacts-connect.html",
+  },
+  {
+    alt: "Time",
+    src: "time.png",
+    href: "https://time.com/3842365/facebook-money-nepal-earthquake/",
+  },
+  {
+    alt: "CNN Money",
+    src: "cnn-money.png",
+    href: "https://money.cnn.com/2015/05/01/media/facebook-nepal-aid/index.html",
+  },
+];
---
+++ main.js
@@ -0,0 +1,169 @@
+// Scroll effects, e.g. photo blur and alignment
+
+// Parallax - one screen height of scroll slides the bg image up fully
+function handleHero() {
+  const scrollTop = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const heroScrollPercent = Math.min(scrollTop / windowHeight, 1);
+  const hero = document.querySelector(".hero");
+  hero.style.backgroundPositionY = `${heroScrollPercent * 100}%`;
+}
+
+// Transition should happen from top of portrait to 100px from bottom
+function handlePortrait() {
+  const windowHeight = window.innerHeight;
+  const polaroids = document.querySelectorAll(".polaroid-container");
+  polaroids.forEach((polaroid) => {
+    polaroid.classList.contains("polaroid-right");
+    const invert = polaroid.classList.contains("polaroid-right") ? -1 : 1;
+    const rect = polaroid.getBoundingClientRect();
+    const scrollPercent = Math.max(
+      0,
+      Math.min(1, (windowHeight - rect.top) / (rect.height - 100))
+    );
+
+    const headshotRotate = invert * (6 - 8 * scrollPercent);
+    const headshotScale = 1 + 0.1 * (1 - scrollPercent);
+    const headshotBlur = 5 * (1 - scrollPercent);
+    polaroid.style.transform = `rotate(${headshotRotate}deg) scale(${headshotScale})`;
+    polaroid.style.filter = `blur(${headshotBlur}px)`;
+    polaroid.style.opacity = `${0.8 + 0.2 * scrollPercent}`;
+  });
+}
+
+// Transition should start when top of header is exposed to 100px from top of screen
+function handleMediaScroller() {
+  const windowHeight = window.innerHeight;
+  const topContent = document.querySelector(".top-content");
+  const mediaHeader = document.querySelector(".media-coverage .header");
+  const experience = document.querySelector(".experience");
+  const topContentRect = topContent.getBoundingClientRect();
+  const mediaHeaderRect = mediaHeader.getBoundingClientRect();
+  const experienceRect = experience.getBoundingClientRect();
+  const scrollPercent = Math.max(
+    0,
+    Math.min(
+      1,
+      (mediaHeaderRect.top - topContentRect.bottom) /
+        (mediaHeaderRect.top - 100)
+    )
+  );
+  const hidden =
+    experienceRect.top <= 0 || topContentRect.bottom >= windowHeight;
+  const mediaSection = document.querySelector(".media-coverage-logos");
+  if (hidden) {
+    mediaSection.style.visibility = "hidden";
+  } else {
+    mediaSection.style.filter = `blur(${(1 - scrollPercent) * 20}px)`;
+    mediaSection.style.opacity = `${scrollPercent}`;
+    mediaSection.style.visibility = "visible";
+  }
+}
+
+// If there are 3 things for example...
+// From 0 to 0.25: first row fades
+// From 0.25 to 0.5: second row fades
+// From 0.5 to 0.75: third fades in
+function getRowVisibility(scrollPercent, num) {
+  const multiples = 1 / (num + 1);
+  const indexAppearing = Math.floor(scrollPercent / multiples);
+  const result = [];
+  for (let i = 0; i < num; i++) {
+    if (i < indexAppearing) {
+      result.push(1);
+    } else if (i === indexAppearing) {
+      // From 0 to 1 of how far along scroll is from nth multiple to n+1th.
+      const amount = (scrollPercent - indexAppearing * multiples) / multiples;
+      result.push(amount);
+    } else {
+      result.push(0);
+    }
+  }
+  return result;
+}
+
+function handleExperience() {
+  const windowHeight = window.innerHeight;
+  const rect = document.querySelector(".experience").getBoundingClientRect();
+  const scrollPercent = Math.max(
+    0,
+    Math.min(1, (windowHeight * 0.75 - rect.top) / rect.height)
+  );
+
+  const rows = document.querySelectorAll(".timeline-row");
+  const visibility = getRowVisibility(scrollPercent, rows.length);
+  for (let i = 0; i < rows.length; i++) {
+    rows[i].style.filter = `blur(${(1 - visibility[i]) * 20}px)`;
+    rows[i].style.opacity = `${visibility[i]}`;
+  }
+}
+
+function onScroll() {
+  handleHero();
+  handlePortrait();
+  handleMediaScroller();
+  handleExperience();
+}
+
+onScroll();
+document.addEventListener("scroll", onScroll);
+
+function getRandomElements(arr, numElements = 12) {
+  if (arr.length <= numElements) {
+    return arr.slice();
+  }
+
+  const result = [];
+  const usedIndices = new Set();
+
+  while (result.length < numElements) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    if (!usedIndices.has(randomIndex)) {
+      usedIndices.add(randomIndex);
+      result.push(arr[randomIndex]);
+    }
+  }
+
+  return result;
+}
+
+function createRow(randLinks) {
+  const scrollerWrapper = document.createElement("div");
+  scrollerWrapper.className = "scroller-wrapper";
+  const scroller = document.createElement("div");
+  scroller.className = "scroller";
+  scroller.style.marginLeft = `-${Math.floor(Math.random() * 100)}px`;
+  scrollerWrapper.appendChild(scroller);
+
+  const addLink = (link) => {
+    const a = document.createElement("a");
+    a.href = link.href;
+    a.target = "_blank";
+
+    const img = document.createElement("img");
+    img.src = `logos/${link.src}`;
+    img.alt = link.alt;
+
+    a.appendChild(img);
+    scroller.appendChild(a);
+  };
+  // Do it few times for smooth scrolling
+  randLinks.forEach(addLink);
+  randLinks.forEach(addLink);
+  return scrollerWrapper;
+}
+
+function addToScroller() {
+  const mediaSection = document.querySelector(".media-coverage-logos");
+  mediaSection.innerHTML = "";
+  const windowHeight = window.outerHeight;
+  const rows = windowHeight / 50;
+  for (let i = 0; i < rows; i++) {
+    const randLinks = getRandomElements(links);
+    const row = createRow(randLinks);
+    mediaSection.appendChild(row);
+  }
+}
+
+addToScroller();
+window.addEventListener("resize", addToScroller);
